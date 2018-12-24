d3658c56d5d34b96288356160278647ac865afac
caozan <1046248282@qq.com>
2018-12-24 21:05:33 +0800

    新增

---
+++ src/main/java/com/baizhi/TsApplication.java
@@ -0,0 +1,16 @@
+package com.baizhi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.baizhi.dao")
+public class TsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsApplication.class, args);
+    }
+
+}
+
